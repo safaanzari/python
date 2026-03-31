7407125eecc46b1321aeca77c8c076acabda9bcd
safaanzari <anzarisafa@gmail.com>
2026-03-31 21:21:12 +0300

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,8 @@
+def factorial(n):
+    """this is a recursive function find the value of any number"""
+    if X==0 or x==1:
+        return 1
+    else:
+        return x*factorial(x-1)
+print(factorial.__doc__)
+print("the factorial of five is =",factorial(5))
